25793be79ae817b4e4b6b8583c598733d8c70683
Igor Telmenko <izumeroot@gmail.com>
2015-12-22 00:21:16 +0300

    Adding Child Routes Transitioning to Show Only Incomplete Todos Transitioning to Show Only Complete Todos Transitioning back to Show All Todos

--- app/router.js
+++ app/router.js
@@ -6,7 +6,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('todos', { path: '/' });
+  this.route('todos', { path: '/' }, function(){
+      this.route('active');
+      this.route('completed');
+  });
 });
 
 export default Router;
